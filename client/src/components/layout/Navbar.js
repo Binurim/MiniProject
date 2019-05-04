bda111fbc33649ed1458afc35493f26e937a6a49
unknown <binurim95@gmail.com>
2019-05-04 13:35:45 +0530

    navbar colour change

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,16 +5,17 @@ class Navbar extends Component {
     return (
       <div className="navbar-fixed">
         <nav className="z-depth-0">
-          <div className="nav-wrapper white">
+          <div className="nav-wrapper yellow">
             <Link
               to="/"
               style={{
-                fontFamily: "monospace"
+                fontFamily: "monospace",
+            
               }}
               className="col s5 brand-logo center black-text"
             >
-              <i className="material-icons">code</i>
-              MERN
+             {/* <i className="material-icons">code</i> */}
+              MERN APP
             </Link>
           </div>
         </nav>
